e5069661d6c72e84e534683475f0af21b6ed9fa1
elwinspranata <elwinpranata02@gmail.com>
2024-10-12 00:21:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/project1/typedata.kt
@@ -0,0 +1,39 @@
+package com.example.project1
+
+fun main(){
+
+    //byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+    //short
+    val shortValue: Short = 20
+    println("Short: $shortValue")
+
+    //Int
+    val intValue: Int = 30
+    println("Int: $intValue")
+
+    //long
+    val longValue: Long = 40
+    println("Long: $longValue")
+
+    //float
+    val floatValue : Float = 50.6f
+    println("Float: $floatValue")
+
+    //double
+    val charValue: Char = 'A'
+    println(charValue)
+
+    //boolean
+    val booleanValue: Boolean = true
+    println("Boolean: $booleanValue")
+
+    //string
+    var stringValue: String = "Hello, world!"
+    println("String: $stringValue")
+
+    stringValue = "Helo, Kotlin"
+    println("String: $stringValue")
+}
